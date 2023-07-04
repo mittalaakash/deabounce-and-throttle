29e21f372441c5c0daeb283b94ed9e34905c1e90
Mridul <mit.ash.619@gmail.com>
2023-07-04 17:22:35 +0530

    conments added

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
+// Get references to the DOM elements
 const input = document.querySelector('input');
 const defaultText = document.getElementById('default');
 const debounceText = document.getElementById('debounce');
 const throttleText = document.getElementById('throttle');
 
 // Default
+// Event listener for input changes
 input.addEventListener('input', e => {
+  // Update the default text immediately with the input value
   defaultText.textContent = e.target.value;
 
+  // Invoke the debounce and throttle functions with the input value
   onDebounce(e.target.value);
   onThrottle(e.target.value);
 });
 
 // Debounce
+// Debounce function to delay invoking a function until after a specified delay
 const debounce = (fn, delay = 1000) => {
   let timer;
   return function (text) {
@@ -22,20 +27,24 @@ const debounce = (fn, delay = 1000) => {
   };
 };
 
+// Debounce function applied to onDebounce function
 const onDebounce = debounce(text => {
+  // Update the debounce text with the input value after the specified delay
   debounceText.textContent = text;
 });
 
 // Throttle
+// Throttle function to limit the rate at which a function can be invoked
 const throttle = (fn, delay = 1000) => {
   let shouldWait = false;
   let waitingArgs;
 
+  // Function to be called after the specified delay
   const timeoutFunc = () => {
-    console.log('timeoutFunc');
     if (waitingArgs == null) {
       shouldWait = false;
     } else {
+      // Invoke the function with the waiting arguments
       fn(waitingArgs);
       waitingArgs = null;
       setTimeout(timeoutFunc, delay);
@@ -44,10 +53,12 @@ const throttle = (fn, delay = 1000) => {
 
   return function (text) {
     if (shouldWait) {
+      // If waiting, store the arguments for later invocation
       waitingArgs = text;
       return;
     }
 
+    // Invoke the function with the current arguments
     fn(text);
     shouldWait = true;
 
@@ -55,6 +66,8 @@ const throttle = (fn, delay = 1000) => {
   };
 };
 
+// Throttle function applied to onThrottle function
 const onThrottle = throttle(text => {
+  // Update the throttle text with the input value
   throttleText.textContent = text;
 });
